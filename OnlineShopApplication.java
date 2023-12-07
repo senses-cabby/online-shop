090d49573a75c674e479395856cf697aa3d7fcb5
Sophia Davis <153172211+senses-cabby@users.noreply.github.com>
2023-12-07 15:12:01 +0700

    Create OnlineShopApplication.java

---
+++ OnlineShopApplication.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/onlineshop/OnlineShopApplication.java
+package com.example.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopApplication.class, args);
+    }
+}
